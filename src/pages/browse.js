c267537226c5cc6ebee853957975f5ab91cb6bf1
Dilip <dilip7989773@gmail.com>
2022-06-02 18:19:43 +0530

    sec

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,14 @@
 import React from "react";
 import {useContent} from "../hooks";
+import selectionFilter from "../utilis/selection-filter";
 
 export default function Browse () {
     const series = useContent('series')
-    console.log(series);
+    const films = useContent('films')
+
+    const slides = selectionFilter({series, films})
+    console.log(slides);
+    console.log(series)
     return (
         <p>Hello from browse</p>
     )
